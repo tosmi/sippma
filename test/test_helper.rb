a1e2c3a1cc4f68ba80030a545dd05caca020304b
Toni Schmidbauer <toni@stderr.at>
2015-01-09 21:15:35 +0100

    use coveralls in the test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
 require "minitest/reporters"
 Minitest::Reporters.use!
+
+require 'coveralls'
+Coveralls.wear!
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
