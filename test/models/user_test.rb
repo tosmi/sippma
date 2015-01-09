12485d969820b7b658f875351312d99606f6879e
Toni Schmidbauer <toni@stderr.at>
2015-01-09 22:20:40 +0100

    fixed validations and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Max Musterman", email: "max@mustermann.com", password_digest: "", permissions: 1)
+    @user = User.new(username: "max", fullname: "Max Musterman", email: "max@mustermann.com", password_digest: "", permissions: 1)
   end
 
   test "should be valid" do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = ""
+  test "fullname should be present" do
+    @user.fullname = ""
     assert_not @user.valid?
   end
 
@@ -20,8 +20,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-    @user.name = "a" * 51
+  test "fullname should not be too long" do
+    @user.fullname = "a" * 51
     assert_not @user.valid?
   end
 
