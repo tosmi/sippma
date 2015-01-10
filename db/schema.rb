fc455617d5962580c08d3482cdac795633c32627
Toni Schmidbauer <toni@stderr.at>
2015-01-10 21:50:44 +0100

    added  index on username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109211102) do
+ActiveRecord::Schema.define(version: 20150110201532) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20150109211102) do
     t.datetime "updated_at",      null: false
     t.string   "fullname"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
