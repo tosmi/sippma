8294ba20275037175e6ed75cdba9c618662ff266
Toni Schmidbauer <toni@stderr.at>
2015-01-13 22:32:16 +0100

    sample create method to test logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      render :inline =>  '<h1>logged in</h1>'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
