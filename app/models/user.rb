2b34a26b6646ae75d46a42ff96314f867289bfb7
Toni Schmidbauer <toni@stderr.at>
2015-01-14 20:56:34 +0100

    added a digest helper function
    this is used in the users.yml test fixture to generate
    valid passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,10 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6 }
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   has_secure_password
 end
