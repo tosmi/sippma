851890b357c1e77aabd7555468172f1ada1497fb
Toni Schmidbauer <toni@stderr.at>
2015-01-14 19:37:42 +0100

    show a flash when username/password is wrong, redirect on login failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       render :inline =>  '<h1>logged in</h1>'
     else
-      render 'new'
+      flash[:danger] = 'Invalid username/password!'
+      redirect_to root_url
     end
   end
 
