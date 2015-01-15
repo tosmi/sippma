30abb618cdad2fcf4c2fcfc99fbfd9046fa670a3
Toni Schmidbauer <toni@stderr.at>
2015-01-15 22:58:37 +0100

    show a welcome page if there are no patients

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class PatientsControllerTest < ActionController::TestCase
-  test "should get index" do
+  include PatientsHelper
+
+  self.use_transactional_fixtures = true
+
+  test "show welcome if there are no patients" do
     get :index
-    assert_response :success
+    if not have_patients?
+      assert_redirected_to 'no_patients'
+    else
+      assert_redirected_to 'new'
+    end
   end
 
 end
