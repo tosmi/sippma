a03e8a2ba9b114f91b03d10cd79ad7ea09b1a983
Toni Schmidbauer <toni@stderr.at>
2015-01-15 12:11:47 +0100

    integration tests for login/logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @admin = users(:admin)
+  end
+
   test "login with invalid information" do
     get root_url
     assert_template 'sessions/new'
     post login_path, session: { username: '', password: ''}
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?, "flash should not be empty"
     get login_url
@@ -14,7 +19,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do
     get root_path
-    post login_path, session: { username: 'admin', password: 'changeme' }
-    assert_select "h1", "logged in"
+    post login_path, session: { username: @admin.username, password: 'changeme' }
+    assert is_logged_in?
+    assert_redirected_to patients_path
+    get root_path
+    assert_redirected_to patients_path
+    follow_redirect!
+    assert_template 'patients/index'
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
   end
+
 end
