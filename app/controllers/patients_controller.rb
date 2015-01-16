d86896a542674724514ba397d475a658af9acbf4
Toni Schmidbauer <toni@stderr.at>
2015-01-16 17:18:29 +0100

    save a patient record

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -10,6 +10,31 @@ class PatientsController < ApplicationController
   end
 
   def new
+    @patient = Patient.new
   end
 
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      flash[:success] = 'Successfully saved new patient'
+      redirect_to patients_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:firstname,
+                                    :lastname,
+                                    :zip,
+                                    :city,
+                                    :street,
+                                    :ssn,
+                                    :insurance,
+                                    :phonenumber1,
+                                    :phonenumber2,
+                                    :email)
+  end
 end
