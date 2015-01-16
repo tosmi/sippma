d1847128c2c8da46fca6e0c05032c626f4234ba3
Toni Schmidbauer <toni@stderr.at>
2015-01-16 16:41:22 +0100

    rename no_patients to welcome

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,8 @@
 class PatientsController < ApplicationController
-  include PatientsHelper
 
   def index
-    if not have_patients?
-      render 'no_patients'
+    if not Patient.any?
+      render 'welcome'
     end
   end
 
