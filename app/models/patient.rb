a244c105113f381adacc41a321a30e1ff63b58bf
Toni Schmidbauer <toni@stderr.at>
2015-01-16 22:05:04 +0100

    started validations for patients
    testing needed...

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,25 @@
 class Patient < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  VALID_NAME_REGEX = /\A[a-z]+\s([a-z]+\s)*[a-z]+\z/i
+  validates :fristname, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  validates :lastname, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+
+  VALID_ZIP_REGEX = /\A\d{4}\z/
+  validates :zip, presence: true, length: { maximum: 50}, format: { with: VALID_ZIP_REGEX }
+
+  validates :city, presence: true, length: { maximum: 50}
+  validates :street, presence: true, length: { maximum: 50}
+
+  VALID_PHONENUMBER_REGEX = //
+  validates :phonenumber1, presence: true, length: { maximum: 50}, format: { with: VALID_PHONENUMBER_REGEX }
+  validates :phonenumber2, presence: true, length: { maximum: 50}, format: { with: VALID_PHONENUMBER_REGEX }
+  validates :insurance, presence: true, length: { maximum: 50}
+
+  VALID_SSN_REGEX = //
+  validates :ssn, presence: true, length: { maximum: 50}, format: { with: VALID_SSN_REGEX }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
 end
