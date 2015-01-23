955518d192f618b39c7985071c668d94ad86cc61
Toni Schmidbauer <toni@stderr.at>
2015-01-23 23:04:01 +0100

    import integration test for the index page
    covers listing all patients, deleting a patient and showing patient
    details

---
+++ test/integration/patient_index_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class PatientIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @patient_params = {
+      patient: {
+        firstname: 'First',
+        lastname: 'Record',
+        zip: '6666',
+        street: 'Recordstreet 8',
+        city: 'New York',
+        ssn: '4321',
+        insurance: 'WGKK',
+        phonenumber1: '01 123456',
+        phonenumber2: '01 654321',
+        email: 'first@record.com',
+        birthdate: Date.today,
+      }
+    }
+  end
+
+  test "creating the first patient record" do
+    get patients_path
+    assert_match "Welcome to SIPPMA", response.body
+    assert_select "a[href=?]", new_patient_path
+    get new_patient_path
+    assert_difference 'Patient.count', 1 do
+      post patients_path, @patient_params
+    end
+    follow_redirect!
+    assert_select 'div.alert-success', { :count => 1, :text => 'Successfully saved new patient' }
+    assert_select 'td', 'First'
+    assert_select 'td', 'Record'
+    assert_select 'td', '6666 New York, Recordstreet 8'
+    assert_select 'div.btn-toolbar'
+  end
+
+  test "deleting a patient" do
+    assert_difference 'Patient.count', 1 do
+      post patients_path, @patient_params
+    end
+    patient = assigns(:patient)
+    assert patient
+    follow_redirect!
+    assert_select 'td', 'First'
+    assert_difference 'Patient.count', -1 do
+      delete patient_path(patient)
+    end
+  end
+
+  test "show patient details" do
+    assert_difference 'Patient.count', 1 do
+      post patients_path, @patient_params
+    end
+    patient = assigns(:patient)
+    follow_redirect!
+    assert_select 'a[href=?]', patient_path(patient)
+    get patient_path(patient)
+    assert_match 'First Record', response.body
+    assert_match '6666 New York, Recordstreet 8', response.body
+    assert_select 'a[href=?]', patients_path
+    assert_select 'a[href=?]', edit_patient_path(patient)
+  end
+
+end
