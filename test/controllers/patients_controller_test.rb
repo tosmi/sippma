6fd0b5801e0eb80315b40bca719611a0d6fc29c6
Toni Schmidbauer <toni@stderr.at>
2015-01-26 11:39:47 +0100

    the patient now requires a logged in user

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class PatientsControllerTest < ActionController::TestCase
 
   def setup
+    @admin = users(:admin)
     @new_patient = { patient: { firstname: 'Kurt',
                                 lastname: 'Froehlich',
                                 zip: '1234',
@@ -19,6 +20,7 @@ class PatientsControllerTest < ActionController::TestCase
   end
 
   test "show welcome if there are no patients" do
+    log_in_as(@admin)
     get :index
     if not Patient.any?
       assert_template 'welcome'
@@ -28,12 +30,14 @@ class PatientsControllerTest < ActionController::TestCase
   end
 
   test "create a new valid patient" do
+    log_in_as(@admin)
     assert_difference 'Patient.count', 1 do
       post :create, @new_patient
     end
   end
 
   test "does not save patient if invalid" do
+    log_in_as(@admin)
     @new_patient[:patient][:firstname] = ''
     assert_no_difference 'Patient.count' do
       post :create, @new_patient
