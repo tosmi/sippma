3d9d3976a26215bac60b18e277a68b5c7513fb74
Toni Schmidbauer <toni@stderr.at>
2015-01-26 22:15:47 +0100

    changed the default scope to created_at DESC

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,5 +1,6 @@
 class Consultation < ActiveRecord::Base
   belongs_to :patient
+  default_scope -> { order('created_at DESC')}
   validates :patient_id, presence: true
   validates :diagnosis, presence: true, length: { maximum: 200 }
 end
