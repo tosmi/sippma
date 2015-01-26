ebee67372a27d2548e4e8a7b62a3ad4458885089
Toni Schmidbauer <toni@stderr.at>
2015-01-26 11:40:59 +0100

    helper function to log a user in while testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,4 +16,19 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  def log_in_as(user, options = {})
+    password = options[:password]  || 'changeme'
+    if integration_test?
+      post login_path, session: { username: user.username, password: password }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
