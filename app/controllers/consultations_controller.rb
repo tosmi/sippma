d0f393478854e07a4858ab040157215fa4daf324
Toni Schmidbauer <toni@stderr.at>
2015-01-27 21:46:38 +0100

    create action for consultations

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -3,4 +3,21 @@ class ConsultationsController < ApplicationController
     @patient = Patient.find(params[:patient_id])
     @consultation = @patient.consultations.build
   end
+
+  def create
+    @patient = Patient.find(params[:id])
+    @consultation = @patient.consultations.build(consultation_params)
+    if @consultation.save
+      flash[:success] = "Consultation for #{@patient.firstname} #{@patient.lastname} saved!"
+      redirect_to patients_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def consultation_params
+    params.require(:consultation).permit(:content, :diagnosis)
+  end
 end
