b2129a711f9617dbb04a8bf02e330961ccd1271f
Toni Schmidbauer <toni@stderr.at>
2015-01-29 17:07:10 +0100

    cleanup new patient integration tests

--- test/integration/patient_new_test.rb
+++ test/integration/patient_new_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class PatientNewTest < ActionDispatch::IntegrationTest
+  fixtures :users, :patients
+
 
   def setup
     @admin = users(:admin)
+    @max   = patients(:max)
     @new_patient = { patient: { firstname: 'Kurt',
                                 lastname: 'Froehlich',
                                 zip: '1234',
@@ -21,6 +24,7 @@ class PatientNewTest < ActionDispatch::IntegrationTest
 
   test "create a new patient" do
     get new_patient_path
+    assert_redirected_to login_path
     log_in_as(@admin)
     assert_redirected_to new_patient_path
     assert_difference 'Patient.count', 1 do
@@ -29,14 +33,19 @@ class PatientNewTest < ActionDispatch::IntegrationTest
     assert_redirected_to patients_url
     follow_redirect!
     assert_select 'div.alert'
-    assert_select 'div.alert-success'
+    assert_select 'div.alert-success', { :count => 1, :text => 'Successfully saved new patient' }
+    assert_select 'td', 'Kurt'
+    assert_select 'td', 'Froehlich'
+    assert_select 'td', '1234 Wien, Teststrasse 8'
+    assert_select 'td', '4321'
+    assert_select 'div.btn-toolbar'
     delete logout_path
     follow_redirect!
     assert_select 'div.alert', false, "Must not contain success flash after logout"
     assert_select 'div.alert-success', false, "Must not contain success flash after logout"
   end
 
-  test "reject invalid patient" do
+  test "reject creating an invalid patient" do
     get new_patient_path
     log_in_as(@admin)
     assert_redirected_to new_patient_path
@@ -49,5 +58,15 @@ class PatientNewTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
     assert_select 'div.alert-danger'
   end
+
+  test "deleting a patient" do
+    log_in_as(@admin)
+    follow_redirect!
+    assert_select 'td', 'Max'
+    assert_difference 'Patient.count', -1 do
+      delete patient_path(@max)
+    end
+  end
+
 
 end
