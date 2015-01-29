7829bb31e688dfe00d9568f46b571e0db00ccb9f
Toni Schmidbauer <toni@stderr.at>
2015-01-29 17:47:04 +0100

    require a logged in user in consultations controller

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,4 +1,6 @@
 class ConsultationsController < ApplicationController
+  before_action :logged_in_user
+
   def new
     @patient = Patient.find(params[:patient_id])
     @consultation = @patient.consultations.build
