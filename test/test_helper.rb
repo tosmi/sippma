a8d3dce32edd90251208f611eb874210d48edf59
Toni Schmidbauer <toni@stderr.at>
2015-01-29 14:47:00 +0100

    disable loading of all fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,13 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+class MiniTest::Unit::TestCase
+  include FactoryGirl::Syntax::Methods
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   def is_logged_in?
     !session[:user_id].nil?
