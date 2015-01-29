62fd02d737665c134fb80a9c12cc49a9ef6ddd06
Toni Schmidbauer <toni@stderr.at>
2015-01-29 17:47:39 +0100

    use the patients fixture for new consultation test

--- test/integration/consultation_new_test.rb
+++ test/integration/consultation_new_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 
 class ConsultationNewTest < ActionDispatch::IntegrationTest
+  fixtures :users, :patients
 
   def setup
     @user = users(:admin)
+    @max  = patients(:max)
   end
 
   test "creating a new consultation" do
-    get new_patient_consultation_path
+    get new_patient_consultation_path(@max)
     assert_redirected_to login_path
     log_in_as(@user)
-    assert_redirected_to new_patient_consultation_path
+    assert_redirected_to new_patient_consultation_path(@max)
   end
 end
