8bfdb35c84b299cb1a89e1d1e24a667c5d006e7c
Toni Schmidbauer <toni@stderr.at>
2015-01-30 20:14:55 +0100

    diagnosis is not required

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -2,5 +2,5 @@ class Consultation < ActiveRecord::Base
   belongs_to :patient
   default_scope -> { order('created_at DESC')}
   validates :patient_id, presence: true
-  validates :diagnosis, presence: true, length: { maximum: 200 }
+  validates :diagnosis, length: { maximum: 200 }
 end
