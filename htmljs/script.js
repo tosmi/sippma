bfd67ab2bd16dae6894fec96d72c4976d5bacc4d
Toni Schmidbauer <toni@stderr.at>
2015-02-04 22:17:37 +0100

    work on dynamic form

--- htmljs/script.js
+++ htmljs/script.js
@@ -1,1 +1,35 @@
-// alert('test');
+function addEntry() {
+    var entries   = document.getElementsByClassName("entry");
+    var newentry  = entries[0].cloneNode(true);
+    var lastentry = entries[entries.length - 1];
+
+    if(entries.length > 10) {
+	alert('The maximum number of entries is 10');
+	return;
+    }
+
+    lastentry.parentNode.insertBefore(cleanEntry(newentry), lastentry.nextSibling);
+}
+
+function cleanEntry(entry) {
+    var fees = entry.getElementsByClassName("fees");
+    fees[0].value = '';
+
+    return entry;;
+}
+
+function updateTotal() {
+    var idsum = document.getElementById("sum");
+    var fees = document.getElementsByClassName("fees");
+
+    var sum = 0;
+    for (i=0; i < fees.length; i++) {
+	var fee = parseFloat(fees[i].value);
+	if(isNaN(fee)){
+	    continue;
+	}
+	sum += fee;
+    }
+
+    idsum.value = sum;
+}
