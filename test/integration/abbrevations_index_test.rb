7a1908d56317ece3f76ad14e7f2280a61cc93f5b
Toni Schmidbauer <toni@stderr.at>
2015-02-13 13:14:32 +0100

    updated integraton test for abbrevations

--- test/integration/abbrevations_index_test.rb
+++ test/integration/abbrevations_index_test.rb
@@ -10,16 +10,22 @@ class AbbrevationsIndexTest < ActionDispatch::IntegrationTest
 
   test "should show welcome if no abbrevations" do
     Abbrevation.delete_all
-    log_in_as(@admin)
     get abbrevations_path
+    assert_redirected_to login_path
+    log_in_as(@admin)
+    assert_redirected_to abbrevations_path
+    follow_redirect!
     assert_template 'welcome'
     assert_select 'a[href=?]', new_abbrevation_path
     assert_match 'no abbrevations', response.body
   end
 
   test "should show abbrevation list if there are any" do
-    log_in_as(@admin)
     get abbrevations_path
+    assert_redirected_to login_path
+    log_in_as(@admin)
+    assert_redirected_to abbrevations_path
+    follow_redirect!
     assert_template 'index'
     assert_select 'th', 'Abbrevation'
     assert_select 'th', 'Text'
