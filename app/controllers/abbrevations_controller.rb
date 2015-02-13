e019fa8372d688e4f1b3d2500aecff8b7a22894e
Toni Schmidbauer <toni@stderr.at>
2015-02-13 13:12:59 +0100

    abbrevations controller update
    - require a logged in user
    - implemented new/create
    - index also return json

--- app/controllers/abbrevations_controller.rb
+++ app/controllers/abbrevations_controller.rb
@@ -1,9 +1,36 @@
 class AbbrevationsController < ApplicationController
+  before_action :logged_in_user
 
   def index
-    if not Abbrevation.any?
-      render 'welcome'
-    end
+    template = 'index'
+
     @abbrevations = Abbrevation.all
+    template = 'welcome' if @abbrevations.empty?
+    respond_to do |format|
+      format.html { render template}
+      format.json { render json: @abbrevations }
+    end
   end
+
+  def new
+    @abbrevation = Abbrevation.new
+  end
+
+
+  def create
+    @abbrevation = Abbrevation.new(abbrevation_params)
+    if @abbrevation.save
+      flash[:success] = 'Successfully created new abbrevation.'
+      redirect_to abbrevations_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def abbrevation_params
+    params.require(:abbrevation).permit(:abbrev, :text)
+  end
+
 end
