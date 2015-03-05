00e32a8c7ee1a710e768c9ef88724d6a7efa25b1
Toni Schmidbauer <toni@stderr.at>
2015-03-05 20:28:15 +0100

    check receipt number and the date

--- test/models/receipt_test.rb
+++ test/models/receipt_test.rb
@@ -5,7 +5,7 @@ class ReceiptTest < ActiveSupport::TestCase
 
   def setup
     @patient = patients(:max)
-    @receipt = @patient.receipts.build(diagnosis: 'bad', totalfee: 100, receiptnumber: '1-1-1-1970', patient_id: @patient.id )
+    @receipt = @patient.receipts.build(diagnosis: 'bad', totalfee: 100, receiptnumber: '1-1-1-1970', date: '1-1-1970', patient_id: @patient.id )
   end
 
   test "receipt should be valid" do
@@ -20,4 +20,18 @@ class ReceiptTest < ActiveSupport::TestCase
   test "order should be most recent first" do
     assert_equal Receipt.first, receipts(:most_recent)
   end
+
+  test "reject invalid receipt numbers" do
+    invalid_receiptnumber = %w(a-1-1-1970 11-111-1-1970 11-11-111-1970 11-11-11-19701)
+    invalid_receiptnumber.each do |invalidnumber|
+      @receipt.receiptnumber = invalidnumber
+      assert_not @receipt.valid?
+    end
+  end
+
+  test "must have a date" do
+    @receipt.date = nil
+    assert_not @receipt.valid?
+  end
+
 end
