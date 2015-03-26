28be295bd15396430a20985eca76219177f0512e
Toni Schmidbauer <toni@stderr.at>
2015-03-26 17:54:37 +0100

    a first integration test for settings

---
+++ test/integration/settings_edit_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SettingsEditTest < ActionDispatch::IntegrationTest
+  fixtures :users
+
+  def setup
+    @admin = users(:admin)
+  end
+
+  test 'updating settings' do
+    get settings_url
+    assert_redirected_to login_url
+    log_in_as(@admin)
+    assert_redirected_to settings_url
+    follow_redirect!
+    assert_select 'a[href=?]', settings_path
+    assert_template 'settings/show'
+    patch settings_path, setting: {
+           firstname: 'Max'
+         }
+  end
+end
