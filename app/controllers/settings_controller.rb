ea1bd9799f4ced13e882d001a8b7d6b09dad04de
Toni Schmidbauer <toni@stderr.at>
2015-03-26 17:54:27 +0100

    implemented show and update

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,34 @@
 class SettingsController < ApplicationController
+  before_action :logged_in_user
+
+  def show
+    @setting = Setting.instance
+  end
+
+  def update
+    @setting = Setting.instance
+
+    if @setting.update_attributes(setting_params)
+      flash[:success] = 'Sucessfully saved settings'
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def setting_params
+    params.require(:setting).permit(:title,
+                                    :firstname,
+                                    :lastname,
+                                    :zip,
+                                    :city,
+                                    :street,
+                                    :phonenumber,
+                                    :email,
+                                    :initial_receiptnumber)
+  end
+
+
 end
