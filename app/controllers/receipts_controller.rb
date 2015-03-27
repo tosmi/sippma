18d01e6c18602498428886b8720bd3c5c95e71e3
Toni Schmidbauer <toni@stderr.at>
2015-03-27 17:01:11 +0100

    settings are also required for the new receipt view

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -2,5 +2,7 @@ class ReceiptsController < ApplicationController
   def new
     @patient = Patient.find(params[:patient_id])
     @receipt = @patient.receipts.build
+    @settings = Setting.instance
+    @receiptnumber = "#{@settings.current_receiptnumber+1}-#{Date.today.strftime('%Y-%m-%d')}"
   end
 end
