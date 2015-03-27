b5aea20854873c87f6dc5b3abd5b4f550450ca5b
Toni Schmidbauer <toni@stderr.at>
2015-03-27 22:36:24 +0100

    updated test scripts for addentry, use jquery

--- htmljs/script.js
+++ htmljs/script.js
@@ -1,67 +1,67 @@
-"use strict";
+$(document).ready(function () {
+  "use strict";
 
-var at;
-if (!at) {
-    at = {};
-} else if (typeof at != 'object') {
-    throw new Error('at already exists and is not an object.');
-}
+  function addEntry() {
+    // var entries   = document.getElementsByClassName("entry");
+    // var newentry  = entries[0].cloneNode(true);
+    // var lastentry = entries[entries.length - 1];
 
-if(!at.stderr) {
-    at.stderr = {};
-} else if (typeof at.stderr != 'object') {
-    throw new Error('at already exists and is not an object.');
-}
+    // if(entries.length > 10) {
+    //   alert('The maximum number of entries is 10');
+    //   return;
+    // }
 
-function addEntry() {
-    var entries   = document.getElementsByClassName("entry");
-    var newentry  = entries[0].cloneNode(true);
-    var lastentry = entries[entries.length - 1];
+    // lastentry.getElementsByClassName('btn')[0].style.display = 'none';
+    // newentry.getElementsByClassName('btn')[0].style.display = 'inline';
 
-    if(entries.length > 10) {
-        alert('The maximum number of entries is 10');
-        return;
-    }
+    // lastentry.parentNode.insertBefore(cleanEntry(newentry), lastentry.nextSibling);
 
-    lastentry.getElementsByClassName('btn')[0].style.display = 'none';
-    newentry.getElementsByClassName('btn')[0].style.display = 'inline';
+    var entry = $($('.entry').last());
+    var newentry = entry.clone(true, true);
 
-    lastentry.parentNode.insertBefore(cleanEntry(newentry), lastentry.nextSibling);
-}
+    entry.find('#addentry').hide();
+    newentry.find('#fee').val('');
+    newentry.find('#text').val('');
 
-function cleanEntry(entry) {
+    newentry.appendTo('#entries');
+  }
+
+  function cleanEntry(entry) {
     var fees = entry.getElementsByClassName("fees");
     fees[0].value = '';
 
     return entry;;
-}
+  }
 
-function updateTotal() {
+  function updateTotal() {
     var idsum = document.getElementById("sum");
     var fees = document.getElementsByClassName("fees");
 
     var sum = 0;
     for (var i=0; i < fees.length; i++) {
-        var fee = parseFloat(fees[i].value);
-        if(isNaN(fee)){
-            continue;
-        }
-        sum += fee;
+      var fee = parseFloat(fees[i].value);
+      if(isNaN(fee)){
+        continue;
+      }
+      sum += fee;
     }
     idsum.value = sum;
-}
+  }
 
-function retrieveAbbrevations() {
+  function retrieveAbbrevations() {
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET","abbrevations.json");
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var obj = JSON.parse(xhr.responseText);
-            //alert(obj[0]);
-        }
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var obj = JSON.parse(xhr.responseText);
+        //alert(obj[0]);
+      }
     };
     xhr.send(null);
-}
+  }
 
-retrieveAbbrevations();
+  $('#addentry').click(addEntry);
+  $('#fee').keyup(updateTotal);
+
+});
