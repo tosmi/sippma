1e878b95d1888530e1636a533bff160fd827526c
Toni Schmidbauer <toni@stderr.at>
2015-03-27 17:01:44 +0100

    test that receiptnumber does not change on page reload

--- test/integration/receipt_new_test_test.rb
+++ test/integration/receipt_new_test_test.rb
@@ -16,7 +16,21 @@ class ReceiptNewTestTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select 'a[href=?]', new_patient_receipt_path(@max)
     get new_patient_receipt_url(@max)
-
+    assert_template 'receipts/new'
   end
+
+  test "receiptnumber only changes on save"  do
+    get patients_url
+    assert_redirected_to login_url
+    log_in_as(@admin)
+    assert_redirected_to patients_url
+    follow_redirect!
+    get new_patient_receipt_url(@max)
+    rno1 = assigns(:receiptnumber)
+    get new_patient_receipt_url(@max)
+    rno2 = assigns(:receiptnumber)
+    assert_equal rno1,rno2
+  end
+
 
 end
