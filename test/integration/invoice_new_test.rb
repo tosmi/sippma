4edcdb089d8d51fc8b50961653c28467e2993c5f
Toni Schmidbauer <toni@stderr.at>
2015-03-28 20:45:13 +0100

    cleanup integration tests for invoice

--- test/integration/invoice_new_test.rb
+++ test/integration/invoice_new_test.rb
@@ -6,31 +6,48 @@ class InvoiceNewTestTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:admin)
     @max   = patients(:max)
+    @moritz  = patients(:moritz)
   end
 
-  test "create a new invoice" do
-    get patients_url
+  test "patient has link to new invoice" do
+    get patients_url(@max)
     assert_redirected_to login_url
     log_in_as(@admin)
-    assert_redirected_to patients_url
+    assert_redirected_to patients_path(@max)
     follow_redirect!
-    assert_select 'a[href=?]', new_patient_invoice_path(@max)
+    assert_select 'a[href=?]', new_patient_invoice_path(@moritz)
+  end
+
+  test "create a new invoice for patient with consultation" do
+    get new_patient_invoice_url(@max)
+    assert_redirected_to login_url
+    log_in_as(@admin)
+    assert_redirected_to new_patient_invoice_url(@max)
+    follow_redirect!
     get new_patient_invoice_url(@max)
     assert_template 'invoices/new'
   end
 
-  test "invoicenumber only changes on save"  do
-    get patients_url
+  test "create a new invoice for patient without consultation" do
+    get new_patient_invoice_url(@moritz)
     assert_redirected_to login_url
     log_in_as(@admin)
-    assert_redirected_to patients_url
+    assert_redirected_to new_patient_invoice_url(@moritz)
     follow_redirect!
+    get new_patient_invoice_url(@moritz)
+    assert_template 'invoices/new'
+  end
+
+  test "invoicenumber only changes on save"  do
     get new_patient_invoice_url(@max)
+    assert_redirected_to login_url
+    log_in_as(@admin)
+    assert_redirected_to new_patient_invoice_url(@max)
+    follow_redirect!
     rno1 = assigns(:invoicenumber)
     get new_patient_invoice_url(@max)
     rno2 = assigns(:invoicenumber)
     assert_equal rno1,rno2
   end
-
 
 end
