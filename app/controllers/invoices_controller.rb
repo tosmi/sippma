d313f9f76a760af1aa55e4cfa36cf86eca320227
Toni Schmidbauer <toni@stderr.at>
2015-03-28 20:41:42 +0100

    require a logged in user, define empty create action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,9 +1,14 @@
 class InvoicesController < ApplicationController
+  before_action :logged_in_user
+
   def new
     @patient = Patient.find(params[:patient_id])
     @invoice = @patient.invoices.build
     @settings = Setting.instance
     @invoicenumber = "#{Setting.new_invoicenumber}-#{Date.today.strftime('%d-%m-%y')}"
     @consultation = @patient.consultations.first
+  end
+
+  def create
   end
 end
