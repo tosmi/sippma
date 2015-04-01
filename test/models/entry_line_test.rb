c43a91cd936be4bf8313e78372d570eb67e4e2e3
Toni Schmidbauer <toni@stderr.at>
2015-04-01 22:26:54 +0200

    added test for entry_lines

--- test/models/entry_line_test.rb
+++ test/models/entry_line_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class EntryLineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :patients, :entry_lines
+
+  def setup
+    @patient = patients(:max)
+    @invoice = @patient.invoices.build
+    @line    = @invoice.entry_lines.build(text: 'the first line',
+                                          fee: 10.25)
+  end
+
+  test "saves a valid entryline" do
+    assert @line.valid?
+  end
+
+  test "text should not be to long" do
+    @line.text = "a" * 51
+    assert_not @line.valid?
+  end
 end
