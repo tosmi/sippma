b9e8a5efea1ea2b896cf1a175c81d4b51d1f7f6e
Toni Schmidbauer <toni@stderr.at>
2015-04-09 18:13:04 +0200

    entry line: validate presence of invoice, use inverse_of

--- app/models/entry_line.rb
+++ app/models/entry_line.rb
@@ -1,7 +1,7 @@
 class EntryLine < ActiveRecord::Base
-  belongs_to :invoice
+  belongs_to :invoice, inverse_of: :entry_lines
 
   validates :text, presence: true, length: { maximum: 50 }
   validates_numericality_of :fee
-
+  validates_presence_of :invoice
 end
