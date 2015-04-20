d04d5db23bea879e549e6d8a750f17f69ca319bd
Toni Schmidbauer <toni@stderr.at>
2015-04-20 22:18:37 +0200

    allow the fee to be empty

--- app/models/entry_line.rb
+++ app/models/entry_line.rb
@@ -2,6 +2,6 @@ class EntryLine < ActiveRecord::Base
   belongs_to :invoice, inverse_of: :entry_lines
 
   validates :text, presence: true, length: { maximum: 50 }
-  validates_numericality_of :fee
+  validates_numericality_of :fee, allow_blank: true
   validates_presence_of :invoice
 end
