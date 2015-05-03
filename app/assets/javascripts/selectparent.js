e4cc0ff733e946d8a3ab9853b38e6a3bf967507e
Toni Schmidbauer <toni@stderr.at>
2015-05-03 22:51:38 +0200

    use delegate to add click events to the parents table

--- app/assets/javascripts/selectparent.js
+++ app/assets/javascripts/selectparent.js
@@ -1,24 +1,20 @@
-var makeTRLinks = function(e) {
-  $("tr[data-link]").click(function() {
-    window.location = this.data("link");
-  });
+var selectedParent = function (e) {
+  alert('delegate');
 };
 
-var addEvent = function() {
-  alert('addEvent');
-  $('#parent-modal').on('shown.bs.modal', makeTRLinks);
-};
-
-$(document).ready(function() {
-  alert('doc ready');
-  $('#parent-modal').on('shown.bs.modal', function() {
-    makeTRLinks();
-  });
+var addEvents = function() {
+  alert('addEvents');
 
   $('#add-parent').click(function() {
     $('#parent-modal').modal('show');
     return false;
   });
+
+  $('#parent-table').delegate('tr','click', selectedParent);
+};
+
+$(document).ready(function() {
+  addEvents();
 });
 
-$(document).on('page:load', addEvent);
+$(document).on('page:load', addEvents);
