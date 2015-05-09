efa3fea1aa0a5588e56878a8eae6947a9a8307b8
Toni Schmidbauer <toni@stderr.at>
2015-05-09 21:50:28 +0200

    import relationships controller

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,26 @@
+class RelationshipsController < ApplicationController
+
+  def create
+    @patient = Patient.find(params[:patient_id])
+    @relationship = @patient.relationships.build(relationship_params)
+    if @relationship.save
+      flash[:success] = "Parent added"
+    else
+      flash[:notice]  = "Unable to add parent"
+    end
+    redirect_to 'patients/edit'
+  end
+
+  def destroy
+    @patient = Patient.find(params[:patient_id])
+    @relationship = @patient.relationships.find(params[:parent_id]).destroy
+    flash[:success] = "Parent removed"
+    render 'patients/edit'
+  end
+
+  private
+
+  def relationship_params
+    params.require(:relationships).permit(:parent_id)
+  end
+end
---
+++ test/controllers/relationships_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RelationshipsControllerTest < ActionController::TestCase
+  fixtures :users, :patients
+
+  def setup
+    @admin = users(:admin)
+    @max   = patients(:max)
+    @silke = patients(:silke)
+  end
+
+  test "adding a relationship" do
+    assert_difference 'Relationship.count', 1 do
+      post :create, patient_id: @max, relationships: {
+             parent_id: @silke
+           }
+    end
+    assert @max.child_of?(@silke)
+    assert @silke.parent_of?(@max)
+  end
+
+end
