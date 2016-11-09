cdad86d538103dcdb88f780db0c72a951761e4ec
Toni Schmidbauer <toni@stderr.at>
2016-11-09 21:33:11 +0100

    remove length restriction on consultation

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -2,5 +2,4 @@ class Consultation < ActiveRecord::Base
   belongs_to :patient
   default_scope -> { order('created_at DESC')}
   validates :patient_id, presence: true
-  validates :diagnosis, length: { maximum: 200 }
 end
